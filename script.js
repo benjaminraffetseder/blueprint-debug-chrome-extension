0d6680738a7e3879f07f7f39deebc8bdf2a2745b
Benjamin Raffetseder <benjamin.raffetseder1@gmail.com>
2019-02-01 09:31:19 +0100

    script updated to es6

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 window.onload = () => {
-    const styles = ['blueprint', 'night', 'light', 'reset'];
-    styles.forEach((style) => {
-        console.log(style);
-        document.getElementById(style).onclick = () => {
-            chrome.tabs.insertCSS({ file: 'smooth.css' });
-            chrome.tabs.insertCSS({ file: `${style}.css` });
-        }
-    });
+  const styles = ["blueprint", "night", "light", "reset"];
+  styles.forEach(style => {
+    document.getElementById(style).onclick = () => {
+      // chrome.tabs.insertCSS({ file: 'smooth.css' });
+      chrome.tabs.insertCSS({ file: `${style}.css` });
+    };
+  });
 };
